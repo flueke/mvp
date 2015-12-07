be059098d00369c9b9fbdb234380da8dae30c5d9
Florian Lüke <florianlueke@gmx.net>
2015-12-07 16:25:23 +0100

    wip: trying to get the file_dialog unittest to work

--- external/libmvp/src/libmvp.pro
+++ external/libmvp/src/libmvp.pro
@@ -1,9 +1,4 @@
 TEMPLATE = lib
-
-QT += core gui widgets
-
-CONFIG += debug_and_release c++11
-
-TARGET = libmvp
+TARGET = mvp
 
 include($$PWD/libmvp.pri)
--- external/libmvp/test/testmain.cc
+++ external/libmvp/test/testmain.cc
@@ -1,16 +1,32 @@
 #include "tests.h"
 
+#ifdef RUN_GUI_TESTS
+#include "file_dialog.h"
+#endif
+
 #include <memory>
 #include <QtTest/QtTest>
 
 int main(int argc, char *argv[])
 {
-  std::list<std::shared_ptr<QObject>> tests = {
-    std::make_shared<TestQtExceptionPtr>(),
-    std::make_shared<TestFlash>()
-  };
+  {
+    std::list<std::shared_ptr<QObject>> tests = {
+      std::make_shared<TestQtExceptionPtr>(),
+      std::make_shared<TestFlash>()
+    };
 
-  for (auto obj: tests) {
-    QTest::qExec(obj.get(), argc, argv);
+    for (auto obj: tests) {
+      QTest::qExec(obj.get(), argc, argv);
+    }
   }
+
+#ifdef RUN_GUI_TESTS
+  QApplication app(argc, argv);
+  mvp::FileDialog dialog;
+  dialog.show();
+
+  return app.exec();
+#else
+  return 0;
+#endif // RUN_GUI_TESTS
 }
