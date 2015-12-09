cf2a85a88b0e74118f1c718fae58468edd64356b
Florian Lüke <florianlueke@gmx.net>
2015-12-09 09:58:01 +0100

    wip: getting the file dialog to work

--- src/file_dialog.cc
+++ src/file_dialog.cc
@@ -1,5 +1,8 @@
 #include "file_dialog.h"
 #include <QDebug>
+#include <QEvent>
+#include <QDialogButtonBox>
+#include <QPushButton>
 
 namespace mvp
 {
@@ -10,9 +13,25 @@ FileDialog::FileDialog(QWidget *parent)
   setFileMode(QFileDialog::Directory);
   setNameFilter("ZIP files (*.zip)");
 
-  for (auto child: findChildren<QWidget *>()) {
-      qDebug() << child;
+  get_open_button()->installEventFilter(this);
+}
+
+bool FileDialog::eventFilter(QObject *watched, QEvent *event)
+{
+  auto open_button = get_open_button();
+
+  if (open_button == watched
+    && event->type() == QEvent::EnabledChange) {
+    open_button->setEnabled(true);
   }
+
+  return QFileDialog::eventFilter(watched, event);
+}
+
+QPushButton *FileDialog::get_open_button() const
+{
+  return findChild<QDialogButtonBox *>()->button(
+    QDialogButtonBox::Open);
 }
 
 } // ns mvp
