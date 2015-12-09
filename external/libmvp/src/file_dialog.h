8ca7300406166b28c939c4c50088f8329bda67fe
Florian Lüke <florianlueke@gmx.net>
2015-12-09 09:58:01 +0100

    wip: getting the file dialog to work

--- external/libmvp/src/file_dialog.h
+++ external/libmvp/src/file_dialog.h
@@ -13,7 +13,10 @@ class FileDialog: public QFileDialog
   public:
     FileDialog(QWidget *parent = nullptr);
 
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
   private:
+    QPushButton *get_open_button() const;
 };
 
 } // ns mvp
