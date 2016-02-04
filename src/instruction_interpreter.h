f11265747ea02da771f0726ac9cf89078a5eabbc
Florian Lüke <florianlueke@gmx.net>
2016-02-04 17:49:28 +0100

    generate mem for OTP area instead of executing instruction one-by-one

--- src/instruction_interpreter.h
+++ src/instruction_interpreter.h
@@ -11,8 +11,12 @@ namespace mvp
 class Flash;
 
 void run_instructions(const InstructionList &instructions, Flash *m_flash, uchar subindex,
-    int address_offset = 0);
+    size_t address_offset = 0);
 void print_actions(const InstructionList &instructions);
+
+QVector<uchar> generate_memory(
+      const InstructionList &instructions,
+      size_t address_offset = 0);
 
 } // ns mvp
 } // ns mesytec
