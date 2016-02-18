1a69582250bf6db9b46ccc0f3ed6a46ffcf6a552
Florian Lüke <f.lueke@mesytec.com>
2016-02-18 14:48:50 +0100

    libmvp-split: refactoring: subindex -> section

--- src/firmware_ops.h
+++ src/firmware_ops.h
@@ -25,13 +25,13 @@ class FirmwareWriter: public QObject
 
     void write();
 
-    bool do_erase() const       { return m_do_erase; }
-    bool do_program() const     { return m_do_program; }
-    bool do_verify() const      { return m_do_verify; }
+    bool do_erase() const   { return m_do_erase; }
+    bool do_program() const { return m_do_program; }
+    bool do_verify() const  { return m_do_verify; }
 
-    void set_do_erase(bool b)       { m_do_erase = b; }
-    void set_do_program(bool b)     { m_do_program = b; }
-    void set_do_verify(bool b)      { m_do_verify = b; }
+    void set_do_erase(bool b)   { m_do_erase = b; }
+    void set_do_program(bool b) { m_do_program = b; }
+    void set_do_verify(bool b)  { m_do_verify = b; }
 
   private:
     void write_part(const FirmwarePartPtr &pp,
