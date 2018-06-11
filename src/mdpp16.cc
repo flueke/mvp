e2dbe6d0c8641d501c2c089b829f9eeaca09fb0d
Florian Lüke <f.lueke@mesytec.com>
2018-06-11 15:14:19 +0200

    untested: add mdpp32 calibration data support

--- src/mdpp16.cc
+++ src/mdpp16.cc
@@ -9,6 +9,7 @@ namespace mesytec
 {
 namespace mvp
 {
+
 namespace mdpp16
 {
 
@@ -88,5 +89,86 @@ void format_prediffs(const gsl::span<uchar> data, QTextStream &out)
 }
 
 } // ns mdpp16
+
+namespace mdpp32
+{
+
+void format_calibration_data(const gsl::span<uchar> data, QTextStream &out)
+{
+  out << "offsets:" << endl;
+  format_offsets(data, out);
+
+  out << "prediffs:" << endl;
+  format_prediffs({ std::begin(data) + offsets::total_bytes, std::end(data) }, out);
+}
+
+void format_offsets(const gsl::span<uchar> data, QTextStream &out)
+{
+  using namespace offsets;
+
+  Expects(data.size() >= total_bytes);
+
+  for (size_t channel=0; channel<n_channels; ++channel) {
+
+    for (size_t gain=0; gain<n_gains_total; ++gain) {
+      if (gain >= n_gains_used)
+        continue;
+
+      out << "c=" << qSetFieldWidth(2) << channel << qSetFieldWidth(0)
+        << " g=" << gain << ": ";
+
+      for (size_t prediff=0; prediff<n_prediffs_total; ++prediff) {
+        if (prediff >= n_prediffs_used)
+          continue;
+
+        size_t word_offset = (prediff | (gain << 3) | (channel << 7));
+        size_t byte_offset = word_offset * word_bytes;
+
+        uint32_t value = boost::endian::little_to_native(
+            *(reinterpret_cast<uint32_t *>(data.data() + byte_offset)));
+
+        out << qSetFieldWidth(6) << value << qSetFieldWidth(0);
+
+        if (prediff == n_prediffs_used-1)
+          out << endl;
+        else
+          out << " ";
+      }
+    }
+  }
+}
+
+void format_prediffs(const gsl::span<uchar> data, QTextStream &out)
+{
+  using namespace prediffs;
+
+  Expects(data.size() >= total_bytes);
+
+  for (size_t channel=0; channel<n_channels; ++channel) {
+
+    out << "c=" << qSetFieldWidth(2) << channel << qSetFieldWidth(0) << ": ";
+
+    for (size_t prediff=0; prediff<n_prediffs_total; ++prediff) {
+      if (prediff >= n_prediffs_used)
+        continue;
+
+      size_t word_offset = (prediff | (channel << 2));
+      size_t byte_offset = word_offset * word_bytes;
+
+      uint32_t value = boost::endian::little_to_native(
+          *(reinterpret_cast<uint32_t *>(data.data() + byte_offset)));
+
+      out << qSetFieldWidth(6) << value << qSetFieldWidth(0);
+
+      if (prediff == n_prediffs_used-1)
+        out << endl;
+      else
+        out << " ";
+    }
+  }
+}
+
+} // ns mdpp32
+
 } // ns mvp
 } // ns mesytec
--- src/mdpp16.h
+++ src/mdpp16.h
@@ -54,6 +54,70 @@ namespace mdpp16
   void format_prediffs(const gsl::span<uchar> data, QTextStream &out);
 
 } // ns mdpp16
+
+namespace mdpp32
+{
+  const size_t n_channels   = 32;
+  const size_t channel_bits =  5;
+  const size_t word_bytes   =  4;
+
+  // offset values organized like this:
+  // offsets[channel][gain][prediff]
+  // offsets[32][10][5]
+  // empty slots need to be skipped
+  namespace offsets
+  {
+    const size_t gain_bits          =  4;
+    const size_t n_gains_used       =  5;
+    const size_t n_gains_total      = 1u << gain_bits;
+
+    const size_t prediff_bits       =  3;
+    const size_t n_prediffs_used    =  5;
+    const size_t n_prediffs_total   = 1u << prediff_bits;
+
+    const size_t total_bits         = channel_bits + gain_bits + prediff_bits;
+    const size_t total_bytes        = (1u << total_bits) * word_bytes;
+  }
+
+  // prediff values organized like this:
+  // prediffs[channel][#prediff]
+  // prediffs[16][3]
+  // empty slots need to be skipped
+  namespace prediffs
+  {
+    const size_t prediff_bits     = 2;
+    const size_t n_prediffs_used  = 4;
+    const size_t n_prediffs_total = 1u << prediff_bits;
+    const size_t total_bits       = channel_bits + prediff_bits;
+    const size_t total_bytes      = (1u << total_bits) * word_bytes;
+  }
+
+  const size_t calib_data_size = offsets::total_bytes + prediffs::total_bytes;
+
+  void format_calibration_data(const gsl::span<uchar> data, QTextStream &out);
+  void format_offsets(const gsl::span<uchar> data, QTextStream &out);
+  void format_prediffs(const gsl::span<uchar> data, QTextStream &out);
+
+} // ns mdpp32
+
+template<typename MDPP>
+void format_calibration_data(const gsl::span<uchar> data, QTextStream &out)
+{
+    return MDPP::format_calibration_data(data, out);
+}
+
+template<typename MDPP>
+void format_offsets(const gsl::span<uchar> data, QTextStream &out)
+{
+    return MDPP::format_offsets(data, out);
+}
+
+template<typename MDPP>
+void format_prediffs(const gsl::span<uchar> data, QTextStream &out)
+{
+    return MDPP::format_prediffs(data, out);
+}
+
 } // ns mvp
 } // ns mesytec
 
