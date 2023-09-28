aede1c485c37669459ddec7f5d5ba296ddc29b43
Florian Lüke <f.lueke@mesytec.com>
2023-09-28 14:25:18 +0200

    setup spdlog, adapt to libmvp changes

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,10 @@
 #include <QAction>
 #include <QApplication>
 #include <QLocale>
+#include <QMenu>
+#include <QTextBrowser>
 #include <mesytec-mvlc/util/logging.h>
+#include <spdlog/sinks/qt_sinks.h>
 
 #include "gui.h"
 #include "git_version.h"
@@ -28,8 +31,27 @@ int main(int argc, char *argv[])
   mesytec::mvlc::set_global_log_level(spdlog::level::info);
 
   mvp::MVPLabGui gui;
-  // Hide the "advanced" widget
-  gui.getToggleAdvancedAction()->setChecked(false);
+
+  #if 1
+  // This way of setting up the qt sink for loggers actually does work. Care has
+  // to be taken with the push_back() of the the sink: it's not thread-safe!
+  //const auto MaxLines = 1u << 20;
+  //auto sink = std::make_shared<spdlog::sinks::qt_color_sink_mt>(gui.getLogview(), MaxLines);
+  auto sink = std::make_shared<spdlog::sinks::qt_sink_mt>(gui.getLogview(), "append");
+  const auto LoggerNames = { "mvlc", "mvlc_eth", "mvlc_usb", "mvlc_mvp_lib" };
+
+  if (auto logger = mesytec::mvlc::default_logger())
+    logger->sinks().push_back(sink);
+
+  for (const auto &ln: LoggerNames)
+    if (auto logger = mesytec::mvlc::get_logger(ln))
+      logger->sinks().push_back(sink);
+  #endif
+
+  // Hide the "advanced" widget and the view menu. This is the only thing making
+  // 'mvp' different from 'mvplab'.
+  gui.findChild<QAction *>("actionShowAdvanced")->setChecked(false);
+  gui.findChild<QMenu *>("menu_View")->deleteLater();
   gui.show();
 
   return app.exec();
